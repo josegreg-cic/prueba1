0ad5f51a9a6b98cc1d267cedfbae8382d2f2c51a
=josegreg-cic <=jose_greg@hotmail.com>
2018-08-23 21:00:31 -0500

    primer archivo py

---
+++ scratch_1.py
@@ -0,0 +1,18 @@
+### funciones elementales
+
+def sumar(num1, num2):
+    return float(num1) + float(num2)
+
+def restar(num1, num2):
+    return float(num1) -float(num2)
+
+def multiplicar(num1,num2):
+    return float(num1)*float(num2)
+
+def dividir(num1,num2):
+    return float(num1)/float(num2)
+
+
+print(sumar(2,1))
+print(multiplicar(3,5))
+print(dividir("3","2"))
